959aba25620d39af188454a2d850a8a1d8cc98d0
shrilakshmishastry <shrilakshmishastry@gmail.com>
2022-09-12 20:59:12 +0530

    add virtualization

--- components/Ideas/Card.js
+++ components/Ideas/Card.js
@@ -2,33 +2,46 @@ import styles from '../../styles/idea.module.css';
 import UserAvatar from '../../images/user_avatar.png';
 import Image from 'next/image';
 
-const Card = () => {
+const Card = ({ data, style }) => {
+  const bgColor = data.status === 'Submitted' ?
+    styles.statusBtnSubmitted :
+    data.status === 'Not Started' ?
+      styles.statusBtnNotStarted :
+      styles.statusBtnInProgress
   return (
-    <div className={`${styles.card} text-black bg-white grid grid-rows-4 grid-cols-1`}>
+    <div style={style} className={`${styles.card} text-black bg-white grid grid-rows-4 grid-cols-1`}>
       <div className=" items-center justify-items-center grid grid-rows-1 grid-cols-2 " >
         <h3 className=" text-3xl font-semibold">
-          Idea title
+          {data.title}
         </h3>
-        <Image
-          className="justify-self-center"
-          height={70}
-          width={70}
-          layout={'fixed'}
-          src={require('/images/user_avatar.png')}
-          alt={'user avatar'} aria-label='user avatar' />
+        <div>
+          {
+            data.avatar_url.map((value, index) => {
+              const styleAvaratr = index === 1 ? `${styles.cardAvatar2nd}  justify-self-center` : `${styles.cardAvatar} justify-self-center`;
+              return (
+                <Image
+                  key={value + index.toString()}
+                  className={data.avatar_url.length > 1 ? styleAvaratr : "cardAvatarSingle"}
+                  height={70}
+                  width={70}
+                  layout={'fixed'}
+                  src={data.avatar_url[0]}
+                  alt={'user avatar'} aria-label='user avatar' />
+              );
+            })
+          }
+
+        </div>
+
       </div>
       <div className="row-span-2 mt-9  ">
         <p className='text-center text-base font-light'>
-          Ea rebum dolore ut est sea et est,
-          amet labore eirmod labore eirmod dolore duo dolores,
-          et gubergren sed invidunt ipsum. Erat diam ipsum eirmod
-          amet lorem. Takimata invidunt erat sit ut et vero.
-          Amet lorem ipsum sea dolores. Sadipscing elitr sea
+          {data.description}
         </p>
       </div>
       <div className=" flex justify-center items-end">
-        <button className=' text-white border-0 rounded h-8 w-32  inline  bg-green-500 '>
-          In progress
+        <button className={` text-white border-0 rounded h-8 w-32  inline ${bgColor}`}>
+          {data.status}
         </button>
       </div>
     </div>
--- pages/ideas.js
+++ pages/ideas.js
@@ -1,9 +1,135 @@
 import Card from "/components/Ideas/Card";
 import styles from '/styles/idea.module.css';
+import { Grid, AutoSizer } from "react-virtualized";
+
+const listOfIdeas = [
+  {
+    id: 1,
+    title: 'Idea title',
+    description: 'Et amet ea dolor vero sed dolore ut et nonumy takimata, no rebum et erat erat dolore lorem, at sit kasd et sea dolor ipsum stet diam, magna diam sea takimata sea sed, no invidunt aliquyam sed dolores at ipsum voluptua, ut lorem et kasd at sed amet dolores consetetur.',
+    avatar_url: [require('/images/user_avatar.png')],
+    user_name: ['Shrilaskhmi', 'Koustov'],
+    status: 'InProgress'
+  },
+  {
+    id: 2,
+    title: 'Idea title2',
+    description: 'Et amet ea dolor vero sed dolore ut et nonumy takimata, no rebum et erat erat dolore lorem, at sit kasd et sea dolor ipsum stet diam, magna diam sea takimata sea sed, no invidunt aliquyam sed dolores at ipsum voluptua, ut lorem et kasd at sed amet dolores consetetur.',
+    avatar_url: [require('/images/user_avatar.png')],
+    user_name: ['Shrilaskhmi', 'Koustov'],
+    status: 'Submitted'
+  },
+  {
+    id: 3,
+    title: 'Idea title2',
+    description: 'Et amet ea dolor vero sed dolore ut et nonumy takimata, no rebum et erat erat dolore lorem, at sit kasd et sea dolor ipsum stet diam, magna diam sea takimata sea sed, no invidunt aliquyam sed dolores at ipsum voluptua, ut lorem et kasd at sed amet dolores consetetur.',
+    avatar_url: [require('/images/user_avatar.png')],
+    user_name: ['Shrilaskhmi', 'Koustov'],
+    status: 'Submitted'
+  },
+  {
+    id: 4,
+    title: 'Idea title2',
+    description: 'Et amet ea dolor vero sed dolore ut et nonumy takimata, no rebum et erat erat dolore lorem, at sit kasd et sea dolor ipsum stet diam, magna diam sea takimata sea sed, no invidunt aliquyam sed dolores at ipsum voluptua, ut lorem et kasd at sed amet dolores consetetur.',
+    avatar_url: [require('/images/user_avatar.png')],
+    user_name: ['Shrilaskhmi', 'Koustov'],
+    status: 'Not Started'
+  },
+  {
+    id: 5,
+    title: 'Idea title2',
+    description: 'Et amet ea dolor vero sed dolore ut et nonumy takimata, no rebum et erat erat dolore lorem, at sit kasd et sea dolor ipsum stet diam, magna diam sea takimata sea sed, no invidunt aliquyam sed dolores at ipsum voluptua, ut lorem et kasd at sed amet dolores consetetur.',
+    avatar_url: [require('/images/user_avatar.png')],
+    user_name: ['Shrilaskhmi', 'Koustov'],
+    status: 'Submitted'
+  },
+  {
+    id: 6,
+    title: 'Idea title2',
+    description: 'Et amet ea dolor vero sed dolore ut et nonumy takimata, no rebum et erat erat dolore lorem, at sit kasd et sea dolor ipsum stet diam, magna diam sea takimata sea sed, no invidunt aliquyam sed dolores at ipsum voluptua, ut lorem et kasd at sed amet dolores consetetur.',
+    avatar_url: [require('/images/user_avatar.png')],
+    user_name: ['Shrilaskhmi', 'Koustov'],
+    status: 'Submitted'
+  },
+  {
+    id: 7,
+    title: 'Idea title2',
+    description: 'Et amet ea dolor vero sed dolore ut et nonumy takimata, no rebum et erat erat dolore lorem, at sit kasd et sea dolor ipsum stet diam, magna diam sea takimata sea sed, no invidunt aliquyam sed dolores at ipsum voluptua, ut lorem et kasd at sed amet dolores consetetur.',
+    avatar_url: [require('/images/user_avatar.png')],
+    user_name: ['Shrilaskhmi', 'Koustov'],
+    status: 'Submitted'
+  },
+  {
+    id: 8,
+    title: 'Idea title2',
+    description: 'Et amet ea dolor vero sed dolore ut et nonumy takimata, no rebum et erat erat dolore lorem, at sit kasd et sea dolor ipsum stet diam, magna diam sea takimata sea sed, no invidunt aliquyam sed dolores at ipsum voluptua, ut lorem et kasd at sed amet dolores consetetur.',
+    avatar_url: [require('/images/user_avatar.png')],
+    user_name: ['Shrilaskhmi', 'Koustov'],
+    status: 'Submitted'
+  },
+  {
+    id: 9,
+    title: 'Idea title2',
+    description: 'Et amet ea dolor vero sed dolore ut et nonumy takimata, no rebum et erat erat dolore lorem, at sit kasd et sea dolor ipsum stet diam, magna diam sea takimata sea sed, no invidunt aliquyam sed dolores at ipsum voluptua, ut lorem et kasd at sed amet dolores consetetur.',
+    avatar_url: [require('/images/user_avatar.png')],
+    user_name: ['Shrilaskhmi', 'Koustov'],
+    status: 'Submitted'
+  },
+  {
+    id: 10,
+    title: 'Idea title2',
+    description: 'Et amet ea dolor vero sed dolore ut et nonumy takimata, no rebum et erat erat dolore lorem, at sit kasd et sea dolor ipsum stet diam, magna diam sea takimata sea sed, no invidunt aliquyam sed dolores at ipsum voluptua, ut lorem et kasd at sed amet dolores consetetur.',
+    avatar_url: [require('/images/user_avatar.png'), require('/images/user_avatar.png')],
+    user_name: ['Shrilaskhmi', 'Koustov'],
+    status: 'Submitted'
+  }
+];
+
+
 const IdeaListingPage = () => {
+
+  const CellRender = (columnIndex, key, rowIndex, style) => {
+    const row = Number(rowIndex.split("-")[0]);
+    const ind = (row * 3) + columnIndex;
+
+    if (ind < listOfIdeas.length) {
+      return (
+        <Card data={listOfIdeas[ind]} style={style} />
+      );
+    }
+    return null;
+
+  }
+  console.log(Math.ceil(listOfIdeas.length / 3));
   return (
-    <main className={styles.container}>
-      <Card />
+    <main className={`${styles.container} `}>
+      <AutoSizer>
+        {
+          ({ width, height }) => {
+            return (
+              <Grid
+                cellRenderer={({ columnIndex, key, rowIndex, style }) => (CellRender(columnIndex, rowIndex, key, style))}
+                columnWidth={380}
+                height={height}
+                containerStyle={{
+                  marginLeft: '5%',
+                  display: 'flex',
+                  justifyContent: 'center'
+                }}
+                columnCount={3}
+                overscanColumnCount={1}
+                overscanRowCount={1}
+                rowHeight={430}
+                rowCount={Math.ceil(listOfIdeas.length / 3)}
+                width={width}
+              />
+
+            );
+          }
+        }
+      </AutoSizer>
+
+
     </main>
 
   );
