3bc8b72a3a23ed978eee6ae7ee414865fd71a39c
Ammaar Aslam <ammaaraslam7@gmail.com>
2022-09-13 23:35:37 +0530

    basic layout of home about section

--- components/Home/About.js
+++ components/Home/About.js
@@ -1,7 +1,30 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="relative flex py-4 px-8 justify-center items-center overflow-clip">
+      <div className="flex flex-row col-span-4 mx-auto">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum
+          est ultricies integer quis. Iaculis urna id volutpat lacus laoreet.
+          Mauris vitae ultricies leo integer malesuada. Ac odio tempor orci
+          dapibus ultrices in. Egestas diam in arcu cursus euismod. Dictum fusce
+          ut placerat orci nulla. Tincidunt ornare massa eget egestas purus
+          viverra accumsan in nisl. Tempor id eu nisl nunc mi
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum
+          est ultricies integer quis. Iaculis urna id volutpat lacus laoreet.
+          Mauris vitae ultricies leo integer malesuada. Ac odio tempor orci
+          dapibus ultrices in. Egestas diam in arcu cursus euismod. Dictum fusce
+          ut placerat orci nulla. Tincidunt ornare massa eget egestas purus
+          viverra accumsan in nisl. Tempor id eu nisl nunc mi
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
