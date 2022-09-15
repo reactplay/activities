7fd5753e28bd1705a14e39656d243c1cf034adc0
Ammaar Aslam <ammaaraslam7@gmail.com>
2022-09-15 06:11:36 +0530

    rearranged zindex of content in judges & mentors section

--- components/Home/Judges.js
+++ components/Home/Judges.js
@@ -14,14 +14,14 @@ const Judges = () => {
           height={155}
         />
       </div>
-      <div className="absolute left-36 -top-16">
+      <div className="absolute left-36 -top-16 ">
         <Image src={Lines} alt="Lines" width={800} height={800} />
       </div>
 
-      <h1 className="font-primary text-white text-5xl tracking-wider">
+      <h1 className="font-primary text-white text-5xl tracking-wider z-10">
         Judges & Mentors
       </h1>
-      <div className="inline-flex justify-center items-center mt-14 p-3">
+      <div className="inline-flex justify-center items-center mt-14 p-3 z-10">
         <div className="mr-6 p-2 flex flex-col justify-center items-start font-body">
           <div className="w-48 h-48 bg-gray-600 rounded-md rounded-tl-[4rem]"></div>
           <span className="text-white">Ipsum dolor sit</span>
