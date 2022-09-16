19f12aa37afd96e3e20d4115cd8a87fdc5eb258c
shrilakshmishastry <shrilakshmishastry@gmail.com>
2022-09-17 00:04:54 +0530

    build new design

--- components/Ideas/Card.js
+++ components/Ideas/Card.js
@@ -1,72 +1,95 @@
 import styles from '../../styles/idea.module.css';
 import Image from 'next/image';
-import { useState } from 'react';
-import { Grid, Card, CardContent } from '@mui/material';
+import { Grid, Card, Typography } from '@mui/material';
+import InProgress from '/public/Idea-List/inProgress.svg';
+import Complted from '/public/Idea-List/completed.svg';
+import NotStarted from '/public/Idea-List/notStart.svg';
 
 
 const IdeaCard = ({ data }) => {
-  const [nameVisibility, setNameVisibility] = useState(false);
-  const bgColor = data.status === 'Submitted' ?
-    styles.statusBtnSubmitted :
+
+  const [image, color] = data.status === 'Submitted' ?
+    [Complted, '#68FDC6'] :
     data.status === 'Not Started' ?
-      styles.statusBtnNotStarted :
-      styles.statusBtnInProgress
+      [NotStarted, '#FD6868'] :
+      data.status === 'In Progress' ?
+        [InProgress, '#FDC668'] : [null, null];
+
 
   return (
-    <Card className={styles.card} sx={{ minWidth: 361, maxWidth: 361, minHeight: 350 }}>
-      <CardContent>
-        <Grid container spacing={{ md: 2 }} columns={{ xs: 2 }} >
-          <Grid item xs={1} md={1} className="flex justify-center ">
-            <h3 className=" text-3xl font-semibold">
-              {data.title}
-            </h3>
+    <Card className={styles.card} variant="outlined">
+      <Grid container
+
+        columns={{ xs: 1 }}
+        className={" pt-12 px-8 h-72"}
+      >
+        <Grid item xs={1} className={""}>
+          <Typography variant='h5' color="#00F2FE">
+            {data.title}
+          </Typography>
+        </Grid>
+        <Grid item xs={1}
+          className="">
+          <div
+            className={styles.cardDescription}
+            dangerouslySetInnerHTML={{ __html: data.description }} />
+        </Grid>
+        <Grid item xs={1}
+          alignSelf={"flex-end"}>
+          <Grid
+            columns={12}
+            container >
+            <Grid item
+              className="flex"
+              xs={6} >
+              {
+                data.avatarUrl.map((value, index) => {
+                  const styleAvaratr = index === 1 ? `${styles.cardAvatar2nd}  justify-self-center` : `${styles.cardAvatar} justify-self-center`;
+                  return (
+                    <>
+                      {
+                        value && (
+                          // <Grid item xs={6}>
+                          <Image
+                            key={value + index.toString()}
+                            className={data.avatarUrl.includes(undefined) ? styles.cardAvatarSingle : styleAvaratr}
+                            height={50}
+                            width={50}
+                            layout={'fixed'}
+                            src={data.avatarUrl[index]}
+                            alt={'user avatar'} aria-label='user avatar' />
+                          // </Grid>
+                        )
+                      }
+                    </>
+                  );
+                })
+              }
+            </Grid>
+            <Grid item
+              xs={6}
+              className="flex flex-row justify-center gap-2 items-center ">
+              <Image src={image !== null ? image : ""} alt={`status ${data.status}`} />
+              <Typography variant={"body2"} color={color}>
+                {data.status}
+              </Typography>
+
+            </Grid>
           </Grid>
-          <Grid item xs={1} md={1} className=" flex justify-end">
-            {
-              data.avatarUrl.map((value, index) => {
-                const styleAvaratr = index === 1 ? `${styles.cardAvatar2nd}  justify-self-center` : `${styles.cardAvatar} justify-self-center`;
-                return (
-                  <Image
-                    onMouseEnter={() => { console.log("hello"); setNameVisibility(true); }}
-                    onMouseLeave={() => setNameVisibility(false)}
-                    key={value + index.toString()}
-                    className={data.avatarUrl.length > 1 ? styleAvaratr : styles.cardAvatarSingle}
-                    height={70}
-                    width={70}
-                    layout={'fixed'}
-                    src={data.avatarUrl[index]}
-                    alt={'user avatar'} aria-label='user avatar' />
-                );
-              })
-            }
-          </Grid>
         </Grid>
-        <Grid container justifyContent={"flex-end"} className='h-8' >
-          {
-            nameVisibility ?
-              data.displayName.map((value, index) => {
-                return (
-                  <Grid item className=" text-base font-semibold" key={value}>
-                    {value}
-                    {data.displayName.length === 2 && index === 0 ? <>,</> : null}
-                  </Grid>
-                );
-              })
-              : <></>
-          }
-        </Grid>
-        <Grid container className=" h-44 mt-4" justifyContent={"center"}>
-          <p className='text-center text-base font-light'>
-            {data.description}
-          </p>
-        </Grid>
-        <Grid container justifyContent={"center"}>
-          <button className={` text-white border-0 rounded h-8 w-32  inline ${bgColor}`}>
-            {data.status}
-          </button>
-        </Grid>
-      </CardContent>
-    </Card>
+      </Grid >
+    </Card >
   );
 }
 export default IdeaCard;
+
+{/* <Grid item xs={1}>
+            <Typography variant='h6'>
+              {data.title}
+            </Typography>
+          </Grid>
+          <Grid item xs={1}>
+            <Typography variant='subtitle2'>
+              {data.title}
+            </Typography>
+          </Grid> */}
--- pages/ideas.js
+++ pages/ideas.js
@@ -1,76 +1,132 @@
 import IdeaCard from "/components/Ideas/Card";
-import styles from '/styles/idea.module.css';
-import { useQuery } from '@apollo/client';
-import ideaQuery from '/queries/fetch-ideas.gql';
 import Layout from "../components/Layout";
 import { useEffect, useState } from "react";
 import { Grid } from "@mui/material";
+import Image from "next/image";
+import { listIdea } from "../services/graphql/ideas";
+import Typography from '@mui/material/Typography';
+import { CTA } from "/components/Hack-R-Play";
+import DottedAndFilledTriangle from "/public/Hack-R-Play/DottedAndFilledTriangle.svg";
+import Flower from "/public/Hack-R-Play/Flower.svg";
+import Logo from "/public/Logo.png";
+import Link from "next/link";
+import styles from '/styles/idea.module.css'
+
 
 
 const IdeaListingPage = () => {
-  const { loading, error, data } = useQuery(ideaQuery);
   const [ideas, setIdeas] = useState([]);
 
-  useEffect(() => {
+  async function dataFetcher() {
+    try {
+      const result = await listIdea();
+      let tempData = [];
+      for (const idea of result) {
 
-    if (data) {
-      const ideas = data.hackathon_idea_status;
-      const members = data.hackathon_ideas_members;
-      let ideaWithMem = [];
-      for (let a of ideas) {
-        let interHolder = {
-          ideaId: a.idea_id,
-          title: a.idea_id_map.title,
-          description: a.idea_id_map.description,
-          status: a.status_id_map.label,
-          displayName: [],
-          avatarUrl: []
+        let interObj = {
+          title: idea.title,
+          description: idea.description,
+          avatarUrl: [idea.idea_members_map?.user_id_map.avatarUrl, idea.idea_owner_map?.avatarUrl],
+          tinkers: [idea.idea_members_map?.user_id_map.displayName, idea.dea_owner_map?.displayName],
+          status: idea.idea_status_map?.status_id_map?.label
         };
-        for (let b of members) {
-          if (b.idea_id === a.idea_id) {
-            interHolder['displayName'].push(b.user_id_map.displayName);
-            interHolder['avatarUrl'].push(b.user_id_map.avatarUrl);
-          }
-        }
-        ideaWithMem.push(interHolder);
-      }
-      setIdeas(ideaWithMem);
-    }
 
-  }, [data])
+        tempData.push(interObj);
+      }
+      setIdeas(tempData);
+    } catch (e) {
+
+    }
+  }
+
+
+
+  useEffect(() => {
+    dataFetcher();
+  }, [])
 
 
 
   return (
     <Layout title="HACK-R-PLAY" description="A hackathon hosted by ReactPlay">
-      {
-        loading ? <p className=" text-white text-center mt-10 font-bold">
-          We are coming ....
-        </p>
-          :
-          <>
-            {
-              ideas.length === 0 ? <p className=" h-screen bg-white text-black  text-center pt-36 font-bold">
-                Brain stroming ideas are coming soon...
-              </p>
-                :
-                <main className="flex justify-center bg-white pt-5 pb-5 pl-4 pr-4">
-                  <Grid container
-                    spacing={{ md: 1 }} rowSpacing={2}
-                    columns={{ xs: 1, md: 2, lg: 3 }}>
-                    {
-                      ideas.map((value) => {
-                        return (
-                          <Grid item className="flex justify-center" xs={1} md={1} >
-                            <IdeaCard data={value} />
-                          </Grid>
-                        );
-                      })
-                    }
-                  </Grid>
-                </main>
-            }</>
-      }
+      <div className="absolute left-8 top-12 md:top-2">
+        <Image
+          src={Logo}
+          alt="React play Logo"
+          width={230}
+          height={50}
+          layout="fixed"
+        />
+      </div>
+      <div className="absolute left-9 -top-10">
+        <Image
+          src={DottedAndFilledTriangle}
+          alt="Dotted And Filled Triangle"
+          width={220}
+          height={220}
+        />
+      </div>
+      <div className="absolute -right-60 -top-48">
+        <Image src={Flower} alt="Flower" width={500} height={500} />
+      </div>
+      <div className={`absolute top-48 md:top-3 right-32 md:right-10 ${styles.registerBtn}`}>
+        <Link href={""}>
+          <a className=" font-semibold  text-lg ">
+            REGISTER NOW
+          </a>
+        </Link>
+      </div>
+      <Grid container
+        columns={{ xs: 12, md: 12 }}
+        alignItems={"center"}
+        className="md:px-10 my-20 "
+        justifyContent={"center"}>
+        <Grid
+          xs={12}
+          md={12}
+
+          items container justifyContent={"center"}>
+          <Grid item xs={12} md={12}>
+            <Typography variant="h4" className="text-center" color={"#ffffff"}>
+              SUBMISSIONS
+            </Typography>
+          </Grid>
+          <Grid item
+            container
+            justifyContent={"center"}
+            xs={12} md={12}>
+            <div className={`${styles.underLine} w-20 mt-2 `}></div>
+          </Grid>
+        </Grid>
+
+        <Grid
+          container
+          item
+          xs={12} md={12}
+          spacing={0}
+          rowSpacing={2}
+          columns={{ xs: 12, md: 12 }}
+          className=" py-16 md:px-10">
+          {ideas.map((value) => {
+            return (
+              <Grid item
+                container
+                justifyContent={"center"}
+                xs={12}
+                md={4}>
+                <IdeaCard data={value} />
+              </Grid>
+            );
+          })}
+        </Grid>
+
+      </Grid>
+      <CTA
+        title="Be a part of the best react event"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum
+            est ultricies integer quis. Iaculis urna id volutpat lacus laoreet."
+      />
     </Layout >
   );
 
