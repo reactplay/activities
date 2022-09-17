9eaea0d05d68e662894aaf34fc18eb5dff39e35d
Ammaar Aslam <ammaaraslam7@gmail.com>
2022-09-17 07:08:34 +0530

    change FAQ section content spacing and sizing

--- components/Hack-R-Play/FAQs.js
+++ components/Hack-R-Play/FAQs.js
@@ -25,27 +25,29 @@ const FAQs = ({ faqs }) => {
       id="faqs"
       className="relative mt-16 flex flex-col justify-center items-center py-24 md:px-16 px-0"
     >
-      <h1 className="font-primary text-white text-5xl tracking-wider">FAQs</h1>
+      <h1 className="font-primary text-white text-5xl tracking-wider relative before:content[''] before:absolute before:w-1/2 before:h-1 before:bottom-0 before:left-1/4 before:border-b-[1px]">
+        FAQs
+      </h1>
       <div className="p-10">
         {faqs.map((faq, index) => (
           <div
             key={index}
-            className="w-full flex-col justify-center items-start mt-3"
+            className="w-full flex-col justify-center items-start my-2 py-3 border-b-2 border-gray-800"
           >
             <div className="inline-flex justify-between items-center w-full">
-              <h2 className="font-primary text-white md:text-3xl text-2xl tracking-wider md:mr-48">
+              <h2 className="font-body font-bold text-white md:text-xl text-lg md:mr-40">
                 {faq.question}
               </h2>
               {active === index ? (
                 <button
-                  className={`font-primary md:text-2xl text-xl text-[#010326] md:p-3 p-2 bg-[#00F2FE] border-2 border-[#00F2FE] rounded-full inline-flex justify-center items-center`}
+                  className={`font-primary md:text-2xl text-xl text-[#010326] md:p-2 p-2 bg-[#00F2FE] border-2 border-[#00F2FE] rounded-full inline-flex justify-center items-center`}
                   onClick={() => toggleAnswer(index)}
                 >
                   <FiMinus />
                 </button>
               ) : (
                 <button
-                  className={`font-primary text-2xl text-[#00F2FE] md:p-3 p-2 border-2 border-gray-500 rounded-full inline-flex justify-center items-center`}
+                  className={`font-primary text-2xl text-[#00F2FE] md:p-2 p-2 border-2 border-gray-500 rounded-full inline-flex justify-center items-center`}
                   onClick={() => toggleAnswer(index)}
                 >
                   <FiPlus />
@@ -57,7 +59,7 @@ const FAQs = ({ faqs }) => {
               ref={contentRef}
               className={
                 active === index
-                  ? `block mt-2 max-w-3xl font-body text-gray-500`
+                  ? `block mt-2 max-w-3xl font-body font-semibold text-sm text-gray-500`
                   : `hidden`
               }
             >
