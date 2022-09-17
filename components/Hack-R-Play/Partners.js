d8dc5e29253babd2e7d0c1cd65a5bea71a7a979f
Ammaar Aslam <ammaaraslam7@gmail.com>
2022-09-17 08:19:25 +0530

    added responsiveness to sponsors section

--- components/Hack-R-Play/Partners.js
+++ components/Hack-R-Play/Partners.js
@@ -7,9 +7,9 @@ const Partners = () => {
   return (
     <section
       id="sponsors"
-      className="relative flex flex-col justify-center items-center py-20 px-16"
+      className="relative flex flex-col justify-center items-center py-20 md:px-16 px-8"
     >
-      <div className="absolute md:right-1 md:top-0 w-44 h-44">
+      <div className="absolute right-1 top-0 md:w-44 md:h-44 w-20 h-20 ">
         <Image
           src={PartnersPolygon}
           alt="Partners Polygon"
@@ -20,7 +20,7 @@ const Partners = () => {
       <h1 className="font-primary text-white text-5xl tracking-wider relative before:content[''] before:absolute before:w-1/4 before:h-1 before:-bottom-2 before:border-b-[3px] before:rounded-sm before:left-24 before:border-[#32F9E5]">
         Our Sponsors
       </h1>
-      <div className="mx-auto mt-5 py-10 w-1/3 h-1/3">
+      <div className="mx-auto mt-5 py-10 md:w-1/3 md:h-1/3 w-1/2 h-1/2">
         <Image src={NhostLogo} alt="Nhost Logo" layout="responsive" />
       </div>
     </section>
