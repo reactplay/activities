e91907e6a0b8f351d3eb9beb3ab20fbc13fb810e
Ammaar Aslam <ammaaraslam7@gmail.com>
2022-09-20 05:58:00 +0530

    fix styles in CTA component

--- components/Hack-R-Play/CTA.js
+++ components/Hack-R-Play/CTA.js
@@ -3,6 +3,7 @@ import { SecondaryButton } from "../Buttons";
 import Image from "next/image";
 import CTAImage from "../../public/Hack-R-Play/CTAImage.svg";
 import { useRouter } from "next/router";
+import { FiPlus } from "react-icons/fi";
 
 const CTA = ({ title, description, image }) => {
   const router = useRouter();
@@ -15,22 +16,26 @@ const CTA = ({ title, description, image }) => {
       id="cta"
       className="relative flex flex-col -mt-14 py-16 md:px-28 px-10 justify-center items-center overflow-clip bg-[#00F2FE]"
     >
-      <div className="inline-flex md:flex-row flex-col justify-center items-center mx-auto max-w-4xl md:px-14 px-6">
-        <div className="w-full h-full">
+      <div className="inline-flex md:flex-row flex-col justify-center items-center mx-auto max-w-4xl md:px-11 px-6">
+        <div className="w-full h-full scale-150">
           <Image
             src={image ? image : CTAImage}
             alt="CTA Image"
             layout="responsive"
           />
         </div>
-        <div className="flex flex-col justify-center items-start md:ml-24 md:mt-0 mt-7">
-          <h1 className="font-primary text-[#010326] text-4xl tracking-wider">
+        <div className="flex flex-col justify-center items-start md:ml-36 md:mt-0 mt-7">
+          <h1 className="font-primary text-[#010326] text-4xl tracking-wider max-w-xs">
             {title}
           </h1>
           <p className="md:mt-7 mt-4 font-body text-gray-600">{description}</p>
-          <div className="mt-5">
+          <div className="mt-7">
             <SecondaryButton handleOnClick={() => redirectToRegistration()}>
               Register Now
+              <FiPlus
+                className="ml-2 my-auto text-[#00F2FE] group-hover:text-[#010326] transition-all duration-300"
+                size={25}
+              />
             </SecondaryButton>
           </div>
         </div>
