48e8c72195f8ee0331669db188972dbe830c569d
Ammaar Aslam <ammaaraslam7@gmail.com>
2022-09-22 21:44:40 +0530

    fix confict in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,24 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import Layout from '@/components/Layout';
-import Image from 'next/image';
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Layout from "@/components/Layout";
+import Image from "next/image";
 import {
-	Hero,
-	About,
-	Judges,
-	Partners,
-	CTA,
-	FAQs,
-} from '../components/Hack-R-Play';
+  Hero,
+  About,
+  Judges,
+  Partners,
+  CTA,
+  FAQs,
+} from "../components/Hack-R-Play";
 
-import DottedAndFilledTriangle from '@/public/Hack-R-Play/DottedAndFilledTriangle.svg';
-import Flower from '@/public/Hack-R-Play/Flower.svg';
+import DottedAndFilledTriangle from "@/public/Hack-R-Play/DottedAndFilledTriangle.svg";
+import Flower from "@/public/Hack-R-Play/Flower.svg";
 
 const PAGE_SIZE = 12;
 
-const HomePage = () => {
-	const router = useRouter();
-
 export default function Home() {
+  const router = useRouter();
+
   const faqs = [
     {
       question: "Why should I perticipate in the Hack-R-Play?",
@@ -95,24 +94,24 @@ export default function Home() {
       title: "Founder, ReactPlay",
     },
   ];
-	useEffect(() => {
-		router.push('/hackrplay/2022/home');
-	}, []);
+  useEffect(() => {
+    router.push("/hackrplay/2022/home");
+  }, []);
 
-	return (
-		<Layout title='ReactPlay presents HACK-R-PLAY'>
-			<div className='absolute md:left-9 -top-10 left-5 z-0 md:w-32 md:h-32 w-24 h-24'>
-				<Image
-					src={DottedAndFilledTriangle}
-					alt='Dotted And Filled Triangle'
-					layout='responsive'
-				/>
-			</div>
-			<div className='absolute md:-right-60 md:-top-48 -right-24 -top-14 z-0 md:w-2/5 md:h-2/5 w-1/2 h-1/2'>
-				<Image src={Flower} alt='Flower' layout='responsive' />
-			</div>
+  return (
+    <Layout title="ReactPlay presents HACK-R-PLAY">
+      <div className="absolute md:left-9 -top-10 left-5 z-0 md:w-32 md:h-32 w-24 h-24">
+        <Image
+          src={DottedAndFilledTriangle}
+          alt="Dotted And Filled Triangle"
+          layout="responsive"
+        />
+      </div>
+      <div className="absolute md:-right-60 md:-top-48 -right-24 -top-14 z-0 md:w-2/5 md:h-2/5 w-1/2 h-1/2">
+        <Image src={Flower} alt="Flower" layout="responsive" />
+      </div>
 
-			<Hero />
+      <Hero />
       <About />
       <Judges judges={judges} />
       <Partners />
@@ -122,8 +121,6 @@ export default function Home() {
         "
       />
       <FAQs faqs={faqs} />
-		</Layout>
-	);
-};
-
-export default HomePage;
+    </Layout>
+  );
+}
