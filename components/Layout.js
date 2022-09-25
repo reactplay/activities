dbe997da32f778b5263ab03390aef40188be703a
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2022-09-25 14:41:27 +0530

    Add missing meta tags and robots.tsx file (#50)

--- components/Layout.js
+++ components/Layout.js
@@ -39,6 +39,9 @@ const Layout = ({ links, children, title, description }) => {
       <Head>
         <title>{title}</title>
         <link rel="icon" href="/favicon.png" />
+        <meta charset="UTF-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <meta
           name="keywords"
           content="ReactPlay, HACK-R-PLAY, ReactJS, Hacakthon"
@@ -75,6 +78,10 @@ const Layout = ({ links, children, title, description }) => {
         <meta
           name="twitter:image"
           content="https://hustles.reactplay.io/og-image.png"
+        />
+        <meta
+          name="twitter:site"
+          content="ReactPlayIO"
         />
         <script
           async
