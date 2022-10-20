549b33bbe27cd7c08cec0f7b976d526558b340c4
Ammaar Aslam <ammaaraslam7@gmail.com>
2022-10-20 20:45:28 +0530

    remove unused code

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
-import { useEffect } from "react";
-import { useRouter } from "next/router";
 import Layout from "@/components/Layout";
 
 export default function Home() {
-  const router = useRouter();
-
   return <Layout title="ReactPlay presents HACK-R-PLAY"></Layout>;
 }
