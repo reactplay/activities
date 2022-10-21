116ded1f50a03c23189f7fcb14c79e919252de41
Ammaar Aslam <96367405+ammaaraslam@users.noreply.github.com>
2022-10-21 06:31:56 +0530

    changed redirection method of home page (#95)
    * changed redirection method of home page
    
    * remove unused code

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,5 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import Layout from '@/components/Layout';
+import Layout from "@/components/Layout";
 
 export default function Home() {
-	const router = useRouter();
-	useEffect(() => {
-		router.push('/hackrplay/2022/home');
-	}, []);
-
-	return <Layout title='ReactPlay presents HACK-R-PLAY'></Layout>;
+  return <Layout title="ReactPlay presents HACK-R-PLAY"></Layout>;
 }
