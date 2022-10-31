3bbf9a46909ca3412dcd0766b74683d0a82ccde6
Koustov <koustov@live.com>
2022-10-31 11:41:27 +0530

    Issue-103 Add blog link to winner card (#104)
    * added blog icon
    
    * updated icons

--- components/Hack-R-Play/Winners.js
+++ components/Hack-R-Play/Winners.js
@@ -3,7 +3,7 @@ import { useEffect, useRef, useCallback } from "react";
 import DottedPattern from "../../public/Hack-R-Play/DottedPattern.svg";
 import Lines from "../../public/Hack-R-Play/Lines.svg";
 import { FaRegFileCode, FaTwitter, FaGithub } from "react-icons/fa";
-import { MdOutlineSource } from "react-icons/md";
+import { MdOutlineSource, MdOutlineArticle } from "react-icons/md";
 import ConfettiGenerator from "confetti-js";
 
 const Winners = ({ winners, mentions }) => {
@@ -118,12 +118,13 @@ const Winners = ({ winners, mentions }) => {
             <div className="inline-flex justify-start items-center">
               <a
                 className="mt-3 inline-flex justify-center items-center font-body text-[#00F2FE]  transition-all duration-150 mr-5"
-                href={`${winner.projectSource}`}
+                href={`${winner.article}`}
                 rel="noreferrer"
                 target="_blank"
               >
-                <MdOutlineSource size={25} />
+                <MdOutlineArticle size={25} />
               </a>
+
               <a
                 className="mt-3 inline-flex justify-center items-center font-body text-[#00F2FE]  transition-all duration-150 mr-5"
                 href={`${winner.twitter}`}
@@ -134,7 +135,7 @@ const Winners = ({ winners, mentions }) => {
               </a>
               <a
                 className="mt-3 inline-flex justify-center items-center font-body text-[#00F2FE]  transition-all duration-150"
-                href={`${winner.github}`}
+                href={`${winner.projectSource}`}
                 rel="noreferrer"
                 target="_blank"
               >
@@ -183,11 +184,11 @@ const Winners = ({ winners, mentions }) => {
               <div className="inline-flex justify-start items-center">
                 <a
                   className="mt-3 inline-flex justify-center items-center font-body text-[#00F2FE]  transition-all duration-150 mr-5"
-                  href={`${mention.projectSource}`}
+                  href={`${mention.article}`}
                   rel="noreferrer"
                   target="_blank"
                 >
-                  <MdOutlineSource size={23} />
+                  <MdOutlineArticle size={23} />
                 </a>
                 <a
                   className="mt-3 inline-flex justify-center items-center font-body text-[#00F2FE]  transition-all duration-150 mr-5"
@@ -199,7 +200,7 @@ const Winners = ({ winners, mentions }) => {
                 </a>
                 <a
                   className="mt-3 inline-flex justify-center items-center font-body text-[#00F2FE]  transition-all duration-150"
-                  href={`${mention.github}`}
+                  href={`${mention.projectSource}`}
                   rel="noreferrer"
                   target="_blank"
                 >
