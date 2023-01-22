24d564a272fd05562389991b4f8fcd6bca281115
Debjit Biswas <debjit012@gmail.com>
2023-01-22 14:52:29 +0000

    About Us Steps Updated

--- components/Hack-R-Play/About.js
+++ components/Hack-R-Play/About.js
@@ -39,19 +39,19 @@ const About = () => {
 
             </li>
             <li className="list-disc mt-1 ml-6">
-            Your play must have at least `#2plays1month` tag added to it while creating.
+              Your play must have at least `#2plays1month` tag added to it while creating.
             </li>
             <li className="list-disc mt-1 ml-6">
-            You can build any play of your choice. If you are looking for ideas, <Link className="text-[#00F2FE] hover:underline" href="https://reactplay.io/ideas" target="_blank" rel="noreferrer"> here are some you can get inspired by</Link>.
+              You can build any play of your choice. If you are looking for ideas, <Link className="text-[#00F2FE] hover:underline" href="https://reactplay.io/ideas" target="_blank" rel="noreferrer"> here are some you can get inspired by</Link>.
             </li>
             <li className="list-disc mt-1 ml-6">
-            You must share a weekly Tweet and/or LinkedIn Post about your journey with the hashtag `#2plays1month` and tag ReactPlay's handle(@ReactPlayIO)
+              You must share a weekly Tweet and/or LinkedIn Post about your journey with the hashtag `#2plays1month` and tag ReactPlay's handle(@ReactPlayIO)
             </li>
             <li className="list-disc mt-1 ml-6">
-            You must <Link className="text-[#00F2FE] hover:underline" href="https://github.com/reactplay/react-play/issues/new/choose" target="_blank" rel="noreferrer">create an issue</Link> on the ReactPlay repository for each pull request.
+              You must <Link className="text-[#00F2FE] hover:underline" href="https://github.com/reactplay/react-play/issues/new/choose" target="_blank" rel="noreferrer">create an issue</Link> on the ReactPlay repository for each pull request.
             </li>
             <li className="list-disc mt-1 ml-6">
-            Please join the [ReactPlay discord server](https://discord.gg/UfYj4MvW9A) to get any help while you are building.
+              Please join the [ReactPlay discord server](https://discord.gg/UfYj4MvW9A) to get any help while you are building.
             </li>
           </ul>
           <a
@@ -67,17 +67,17 @@ const About = () => {
       <div className="md:-mt-32 mt-8">
         <div className="w-fit h-fit border-2 border-[#68FDC6] rounded-tl-[6.5rem] flex justify-center items-center md:py-12 md:px-12 pt-12 pb-7 px-9 md:-ml-10">
           <p className=" text-[#68FDC6] font-primary md:text-3xl text-2xl mx-auto max-w-[8rem] max-h-32">
-            Build an App using React and Nhost
+            Create 2 Plays using React
           </p>
         </div>
         <div className="w-fit h-fit border-2 border-white rounded-tr-[6.5rem] flex justify-center items-center md:py-14 md:px-12 py-7 px-9 md:ml-36 md:-mt-10 ml-20 -mt-3">
           <p className=" text-white font-primary md:text-3xl text-2xl  mx-auto max-w-[8.3rem] max-h-32">
-            Write an article on ReactPlay Blog about it
+            Share your journey publicly
           </p>
         </div>
         <div className="w-fit h-fit border-2 border-[#00F2FE] rounded-br-[6.5rem] flex justify-center items-center md:py-14 md:px-12 py-7 px-9 md:ml-80 md:-mt-10 -mt-3">
           <p className=" text-[#00F2FE] font-primary md:text-3xl text-2xl mx-auto max-w-[8rem] max-h-32">
-            Submit the links to code, app, and article
+            Get the plays reviewed and merged
           </p>
         </div>
       </div>
