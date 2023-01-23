6564897a8012cd2ef1cf809daf8284788c3dc403
aimunNaharr <aimunnaharr@gmail.com>
2023-01-23 20:32:23 +0600

    fixed the responsivenes of banner

--- components/Banner.js
+++ components/Banner.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Banner = () => {
    return (
-      <section className='text-slate-800 flex items-center justify-center min-h-[50vh] text-center bg-cyan-200'>
+      <section className='text-slate-800 px-1 md:px-0 flex items-center justify-center min-h-[50vh] text-center bg-cyan-200'>
          <div className=' flex flex-col gap-4 justify-center items-center'>
-            <h1 className='text-5xl font-bold'>#2playsamonth</h1>
+            <h1 className='md:text-5xl text-3xl font-bold'>#2playsamonth</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, debitis?</p>
-            <button className='bg-slate-800 text-white uppercase px-20 py-3 rounded font-bold hover:bg-slate-700 transition-all'>Get started</button>
+            <button className='bg-slate-800 text-white uppercase md:px-20 px-14 py-3 rounded font-bold hover:bg-slate-700 transition-all'>Get started</button>
          </div>
       </section>
    );
