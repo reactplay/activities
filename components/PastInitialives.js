9fb938cc4d436c39644b944fe5a78b5aa4544469
aimunNaharr <aimunnaharr@gmail.com>
2023-01-24 13:02:57 +0600

    created the pastInitives and eventLayout component

--- components/PastInitialives.js
+++ components/PastInitialives.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-const PastInitialives = () => {
-   return (
-      <div>
-         
-      </div>
-   );
+
+const PastInitialives = ({pastEvents}) => {
+	
+	return (
+		<section className="text-white flex items-center justify-center my-28 w-full ">
+			{/* wrapper */}
+			<div className="flex flex-col w-full">
+				<h1 className="md:text-2xl  text-center mb-16 tracking-wild uppercase">Past Initiatives</h1>
+				<div className="flex flex-col md:flex-row w-full md:w-4/5 mx-auto justify-center items-center gap-6  px-3 md:px-0">
+					{/* article */}
+					{pastEvents.map((event, i) => 
+						<Link href={event.link}><article key={i} className="md:basis-1/3 w-full rounded text-sm text-gray-300 min-h-52 flex flex-col gap-2 bg-cyan-800 max-w-[350px] px-6 py-8 cursor-pointer">
+						<Image src={event.image} alt="Banner Logo" layout="responsive" />
+						<p>{event.description}</p>
+					</article></Link>
+					)}
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default PastInitialives;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,42 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import Layout from '@/components/Layout';
-import Banner from '@/components/Banner';
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Layout from "@/components/Layout";
+import BannerLogo from "../public/Hack-R-Play/BannerLogo.png";
+import Banner from "@/components/Banner";
+import PastInitialives from "@/components/PastInitialives";
+
 
 export default function Home() {
 	const router = useRouter();
 	// useEffect(() => {
 	// 	router.push('/hackrplay/2022/home');
 	// }, []);
+	const recentEvent={
+		name: '#2PlaysAMonth',
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, debitis?'
+	}
+	const pastEvents = [
+		{
+			name: "Hack-R-Play",
+			description: 'ReactPlay brings you the opportunity to take part in the Hackathon and learn from it. Showcase your mindblowing ideas, build projects, and create content - there are also chances to win exciting prizes.',
+			image: BannerLogo,
+			link: `/hackrplay/2022/home`
+		},
+		
+	];
+	const EventLayout = () => {
+		return (
+			<div>
+				<Banner recentEvent={recentEvent}/>
+				<PastInitialives pastEvents={pastEvents}/>
+			</div>
+		);
+	};
+	
 
-	return <Layout title='ReactPlay presents HACK-R-PLAY'>
-<Banner/>
-	</Layout>;
+	return (
+		<Layout title="ReactPlay presents HACK-R-PLAY">
+			<EventLayout/>
+		</Layout>
+	);
 }
