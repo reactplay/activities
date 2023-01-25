43156adff480bfe7096b0853f8054f651689d8e2
Koustov <koustov.maitra@microfocus.com>
2023-01-25 14:26:06 +0530

    moved config out of pages folder

--- pages/events/23/twoplaysamonth/index.js
+++ pages/events/23/twoplaysamonth/index.js
@@ -1,57 +1,57 @@
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
-import Layout from "@/components/Layout";
-import Image from "next/image";
-import { useTheme } from "next-themes";
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import Layout from '@/components/Layout';
+import Image from 'next/image';
+import { useTheme } from 'next-themes';
 // import { Hero, About, Judges, CTA, FAQs } from "@/components/common";
-import About from "@/components/common/About";
-import Judges from "@/components/common/Judges";
-import Hero from "@/components/common/Hero";
-import Partners from "@/components/common/Partners";
-import CTA from "@/components/common/CTA";
-import FAQs from "@/components/common/FAQs";
+import About from '@/components/common/About';
+import Judges from '@/components/common/Judges';
+import Hero from '@/components/common/Hero';
+import Partners from '@/components/common/Partners';
+import CTA from '@/components/common/CTA';
+import FAQs from '@/components/common/FAQs';
 
-import DottedAndFilledTriangle from "@/public/common/DottedAndFilledTriangle.svg";
-import Flower from "@/public/common/Flower.svg";
-import { Config } from "./config";
-import Winners from "@/components/common/Winners";
+import DottedAndFilledTriangle from '@/public/common/DottedAndFilledTriangle.svg';
+import Flower from '@/public/common/Flower.svg';
+import { Config } from '@/services/metadata/twoplaysamonth';
+import Winners from '@/components/common/Winners';
 
 export default function Home() {
-  const router = useRouter();
-  const [mounted, setMounted] = useState(false);
-  const { theme, setTheme } = useTheme("twoplaysamonth");
-  useEffect(() => {
-    setTheme("twoplaysamonth");
-    setMounted(true);
-  }, []);
+	const router = useRouter();
+	const [mounted, setMounted] = useState(false);
+	const { theme, setTheme } = useTheme('twoplaysamonth');
+	useEffect(() => {
+		setTheme('twoplaysamonth');
+		setMounted(true);
+	}, []);
 
-  const winners = [];
-  const mentions = [];
+	const winners = [];
+	const mentions = [];
 
-  if (!mounted) return null;
+	if (!mounted) return null;
 
-  return (
-    <Layout title="ReactPlay : #2Plays-A-Month" metainfo={Config}>
-      <div className="absolute md:left-9 -top-10 left-5 z-0 md:w-32 md:h-32 w-24 h-24">
-        <Image
-          src={DottedAndFilledTriangle}
-          alt="Dotted And Filled Triangle"
-          layout="responsive"
-        />
-      </div>
-      <div className="absolute md:-right-60 md:-top-48 -right-24 -top-14 z-0 md:w-2/5 md:h-2/5 w-1/2 h-1/2">
-        <Image src={Flower} alt="Flower" layout="responsive" />
-      </div>
-      <Hero metainfo={Config} />
-      {Config.completed ? (
-        <Winners winners={winners} mentions={mentions} />
-      ) : null}
-      <About metainfo={Config} />
-      <Judges metainfo={Config} />
-      {Config.partners ? <Partners metainfo={Config} /> : null}
-      {Config.cta ? <CTA metainfo={Config} /> : null}
+	return (
+		<Layout title='ReactPlay : #2Plays-A-Month' metainfo={Config}>
+			<div className='absolute md:left-9 -top-10 left-5 z-0 md:w-32 md:h-32 w-24 h-24'>
+				<Image
+					src={DottedAndFilledTriangle}
+					alt='Dotted And Filled Triangle'
+					layout='responsive'
+				/>
+			</div>
+			<div className='absolute md:-right-60 md:-top-48 -right-24 -top-14 z-0 md:w-2/5 md:h-2/5 w-1/2 h-1/2'>
+				<Image src={Flower} alt='Flower' layout='responsive' />
+			</div>
+			<Hero metainfo={Config} />
+			{Config.completed ? (
+				<Winners winners={winners} mentions={mentions} />
+			) : null}
+			<About metainfo={Config} />
+			<Judges metainfo={Config} />
+			{Config.partners ? <Partners metainfo={Config} /> : null}
+			{Config.cta ? <CTA metainfo={Config} /> : null}
 
-      {Config.faqs ? <FAQs metainfo={Config} /> : null}
-    </Layout>
-  );
+			{Config.faqs ? <FAQs metainfo={Config} /> : null}
+		</Layout>
+	);
 }
---
+++ services/metadata/index.js
@@ -0,0 +1,7 @@
+import { Config as tp_config } from './twoplaysamonth';
+import { Config as hrp_config } from './hackrplay';
+
+export const MetaConfigs = {
+	twoplaysamonth: tp_config,
+	hackrplay: hrp_config,
+};
