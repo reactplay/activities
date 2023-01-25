960b0ca109d2800409420251818ad9801a774463
koustov <koustov@live.com>
2023-01-26 00:07:48 +0530

    incorporated feedback from partner

--- components/Header.js
+++ components/Header.js
@@ -43,6 +43,8 @@ const Header = ({ links, metainfo, secondary = false }) => {
   const redirectToRegistration = () => {
     router.push("/hackrplay/2022/registration");
   };
+  console.error(links);
+  console.error(secondary);
 
   return (
     <>
