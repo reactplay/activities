b2b6eef56736d2053c116afab5041e922321f1d6
Ashrrith Karunaakar <30532534+ashrrithk@users.noreply.github.com>
2023-01-31 14:37:40 +0530

    Fixed #129: Spelling mistake in FAQ section (#130)
    * Corrected the spelling error in faq page
    
    * Delete yarn.lock
    
    * Fixed spelling error in faq page

--- services/metadata/twoplaysamonth.js
+++ services/metadata/twoplaysamonth.js
@@ -23,8 +23,7 @@ export const Config = {
       href: "#faqs",
     },
   ],
-  subtitle:
-    "ReactPlay brings you an opportunity to participate in the month-long drive to learn and contribute to Open Source. Join the #2PlaysAMonth and build two projects(plays) in the month of February. You will learn from expert code reviews while contributing to Open Source - you may also win some exciting prizes.",
+  subtitle: "ReactPlay brings you an opportunity to participate in the month-long drive to learn and contribute to Open Source. Join the #2PlaysAMonth and build two projects(plays) in the month of February. You will learn from expert code reviews while contributing to Open Source - you may also win some exciting prizes.",
   about: {
     texts: [
       "#2PlaysAMonth is an initiative from the ReactPlay community to help you learn, build, and simultaneously contribute to open source throughout the month of February.",
@@ -40,8 +39,7 @@ export const Config = {
       "Additionally, if you spend at least 5 hours streaming on [stackstream](https://www.stack-stream.com/) in the February month, you qualify for additional bonus points(karma) to win.",
       "Share a Tweet and/or LinkedIn Post announcing your participation and PR merge with the hashtag `#2PlaysAMonth` and tag ReactPlay's handle(@ReactPlayIO)",
     ],
-    footer:
-      "Please join the [ReactPlay discord server](https://discord.gg/UfYj4MvW9A) to get any help while you are building. Also, check the [FAQ](https://hustles.reactplay.io/events/23/twoplaysamonth#faqs) section for more details.",
+    footer: "Please join the [ReactPlay discord server](https://discord.gg/UfYj4MvW9A) to get any help while you are building. Also, check the [FAQ](https://hustles.reactplay.io/events/23/twoplaysamonth#faqs) section for more details.",
     highlights: [
       "Complete 2 Plays using ReactJS",
       "Demo your work",
@@ -101,12 +99,13 @@ export const Config = {
     },
     {
       question: "Can we participate as a team?",
-      answer: "Nope. It's an individual event. However, feel free to collaborate with others.",
+      answer:
+        "Nope. It's an individual event. However, feel free to collaborate with others.",
     },
     {
       question: "Can I use anything other than React?",
       answer:
-        "ReactPlay is a platform to help web developers learn ReactJs and build projects. For this initiative, RaectJs is a must.",
+        "ReactPlay is a platform to help web developers learn ReactJs and build projects. For this initiative, ReactJs is a must.",
     },
     {
       question: "Can I submit my old React project as a play?",
@@ -130,14 +129,12 @@ export const Config = {
         "No. However, we encourage you to build and learn publicly. Make sure you add the tag #2PlaysAMonth and ReactPlay handle when you post them on social media.",
     },
     {
-      question:
-        "Is recording a Demo of my work on stackstream a must?",
+      question: "Is recording a Demo of my work on stackstream a must?",
       answer:
         "Yes, we encourage you to share your work publicly and at the same time, get better at public speaking. Please register on stackstream and create your streams for free. Don't forget to add the stream recording link into the readme file of the related play.",
     },
     {
-      question:
-        "What about the 5 hours of streaming a month and bonus points?",
+      question: "What about the 5 hours of streaming a month and bonus points?",
       answer:
         "There is a chance to win bonus points if you spend at least 5 hours streaming on stackstream in the February month. For each  hour, you will get a  bonus point(karma). You can use the stackstream platform for free in the entire duration of the #2PlaysAMonth challenge. You can collaborate, share your learnings, demo, etc. Based on your Karma points you got from your streaming and engagement , you can also win something (even if  you couldn't finish your play). Top 3 highest karma point holder will get 3 exciting prizes from stackstream (the threshold is 100 karma points).",
     },
