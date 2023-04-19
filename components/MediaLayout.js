f49aca2acab34d3fccc5bf1eb5175487e773235b
Abhishek-90 <holaniabhishek90@gmail.com>
2023-04-19 14:51:25 +0530

    Rounded youtube iFrame border

--- components/MediaLayout.js
+++ components/MediaLayout.js
@@ -48,7 +48,7 @@ const MediaLayout = ({ events, reactPlayLive, twitterSpaces, title, id }) => {
             key={el.id}
             width="360"
             height="230"
-            className="mb-10"
+            className="mb-10 b-10 rounded-lg"
             src={el.src}
             title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
