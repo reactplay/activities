2e250205fa5acadab997eb826ac41fa753f10172
Tapas Adhikary <tapas.adhikary@gmail.com>
2023-07-20 18:13:47 +0530

    Made changes in the text

--- services/metadata/hackrplay23.js
+++ services/metadata/hackrplay23.js
@@ -22,30 +22,32 @@ export const Config = {
   ],
   submissionurl: "hackrplay/ideas",
   subtitle:
-    "ReactPlay brings you the opportunity to take part in the Hackathon and learn from it. Showcase your mindblowing ideas, build projects, and create content - there are also chances to win exciting prizes",
+    "ReactPlay brings you the opportunity to take part in the Hackathon and learn from it. Showcase your mindblowing ideas, build projects, and create content - there are also chances to win exciting prizes.",
   about: {
     texts: [
       "Developers are lifetime hackers!",
-      "Hack-R-Play is an initiative from the ReactPlay platform to help you hack, build, learn, and simultaneously contribute open source through #hacktoberfest",
+      "Hack-R-Play is an initiative from the ReactPlay platform to help you hack, build, learn, and simultaneously collaborate, contribute, and win.",
       "All for it? Here are some rules you need to keep in mind:",
     ],
     bullets: [
-      "It must be built in React Ecosystem. (eg - ReactJS, NextJS, RemixJS etc)",
-      "The app must use one or more service of [Nhost backend](https://nhost.io)",
-      "Project source code must be open on github.",
-      "You must deploy the app publicly.",
-      "You must write an article about your journey of building it and publish it on the ReactPlay blog.",
+      "You must register your idea by <strong>27th July 2023</strong>. Be mindful of not submitting a dupplicate idea.",
+      "You can build any project of your choice using any technologies you prefer.",
+      "The project source code must be open on github.",
+      "You must deploy the application publicly.",
+      "You must create content(on any soacila media platform or blog or video) about your participation, building things, etc. Make sure to tag <strong>#hackrplay</strong> and <strong>ReactPlay</strong>",
+      "If your project tech stack is supported by [Bugfender](https://bugfender.com), you must show any usage of their SDK/library",
       "[Read more](https://blog.reactplay.io/announcing-hack-r-play-hackathon-from-react-play) on the ReactPlay repository for each pull request.",
     ],
     footer:
       "Please join the [ReactPlay discord server](https://discord.gg/UfYj4MvW9A) to get any help while you are building.",
     highlights: [
-      "Create 2 Plays using React",
-      "Share your journey publicly",
-      "Get the plays reviewed and merged",
+      "Register Your Idea",
+      "Build Project, Collaborate",
+      "Learn, Share, and Win",
     ],
   },
-  judges: [
+  judges: [],
+  /*judges: [
     {
       name: "Johan Eliasson",
       twitter: "elitasson",
@@ -74,12 +76,12 @@ export const Config = {
       avatar:
         "https://pbs.twimg.com/profile_images/1495457010598309888/zPrTNF4F_400x400.jpg",
     },
-  ],
+  ],*/
   faqs: [
     {
-      question: "Why should I perticipate in the Hack-R-Play?",
+      question: "Why should I perticipate in the Hack-R-Play 2.0?",
       answer:
-        "Do you like learning while building something? How about a platform that provides you with an opportunity to build a full-stack application end-to-end, showcase it to the world, motivate you to create content about it, and help build networking? Hack-R-Play aims precisely the same. Join the Hackathon brought to you by ReactPlay and start building a cool project that we can't wait to see!",
+        "Do you like learning while building something? How about a platform that provides you with an opportunity to build applications end-to-end, showcase it to the world, motivate you to create content about it, and help build networking? Hack-R-Play aims precisely the same. Join the Hackathon brought to you by ReactPlay and start building a cool project that we can't wait to see!",
     },
     {
       question: "Do I need to Register for Hack-R-Play?",
@@ -97,29 +99,24 @@ export const Config = {
         "Not really! However, we suggest you build something valuable that you will keep building even after the Hack-R-Play Event. Rest is entirely up to you.",
     },
     {
-      question: "Can I use anything other than React?",
+      question: "Is Mandatory to use React?",
       answer:
-        "You can use anything related to React and its ecosystem. You can use Next.js, Gatsby, React Native, and even plain React!",
+        "Nope, the technology choice is up to YOU!",
     },
     {
       question: "What do I need to Submit?",
       answer:
-        "When you submit the project after completion, you submit three public links. Link to your project source code, link to your application, and link to the article that explains how you have used React/React-based library/framework with Nhost to build this application.",
+        "When you submit the project after completion, you submit three public links. Link to your project source code, link to your application, and link to the content that explains your journey in building this application.",
     },
     {
-      question: "Is the blog article mandatory to submit?",
+      question: "When and How the Hack-R-Play 2.0 result will be published?",
       answer:
-        "Yes, very much! Please document your entire Hack-R-Play journey and publish them on the ReactPlay blog(blog.reactplay.io). Don't worry if you are not a seasoned blogger. We will help with reviews if you submit the blog article at least 3 days before the event's end date.",
-    },
-    {
-      question: "When and How the Hack-R-Play result will be published?",
-      answer:
-        "Hack-R-Play 2022 results will be published on 30th October. We will publish it on our website, and the Twitter handle(@reactplayio).",
+        "Hack-R-Play 2.0 results will be published on 15th August 2023. We will publish it on our website, discord, and our social media handles.",
     },
     {
       question: "Do we get prizes?",
       answer:
-        "Yes, we will select 3 best ideas to give the exciting prizes sponsored by Nhost.",
+        "Yes, we will select 3 best ideas to give the exciting prizes sponsored by Bugfender. We will also give prizes to 2 special mention projects.",
     },
     {
       question: "I have questions, where can I ask them?",
@@ -130,98 +127,20 @@ export const Config = {
   cta: {
     title: "Be a part of the best react event",
     description:
-      "Learning is a journey than a destination. We developers need avenues, motivations, and opportunities to keep going. Join the Hack-R-Play hackathon to experience it. It will allow you to build a full-stack app using React and Nhost. Why waiting? Register your idea today",
+      "Learning is a journey than a destination. We developers need avenues, motivations, and opportunities to keep going. Join the Hack-R-Play hackathon to experience it. Build something awesome that this world is waiting to see. Why waiting? Register your idea today.",
   },
   partners: [
     {
-      logo: "NhostLogo.svg",
-      text: "Nhost is the open source GraphQL backend (Firebase Alternative) and a development platform. Nhost is doing for the backend, what Netlify and Vercel are doing for the frontend. It make things easy to build and deploy this backend using our platform that takes care of configuration, security, and performance. Things just works andscale automatically so you can focus on your product and on your business.",
-      display: "NHost",
-      link: "https://docs.nhost.io/",
+      logo: "bugfender-rgb.png",
+      text: "Bugfender is a log storage service for application developers. Bugfender collects everything happening in the application, even if it does not crash, in order to reproduce and resolve bugs more effectively and provide better customer support.",
+      display: "Bugfender",
+      link: "https://bugfender.com/",
     },
   ],
   winners: [
-    {
-      name: "Chakri",
-      twitter: "https://twitter.com/geekyChakri",
-      github: "https://github.com/GeekyChakri",
-      email: "geekychakri@gmail.com",
-      avatar:
-        "https://pbs.twimg.com/profile_images/1532395278107435011/jBTuAENH_400x400.png",
-      projectName: "Classroom",
-      projectLink: "https://learnwithclassroom.vercel.app",
-      article:
-        "https://blog.reactplay.io/introducing-classroom-learning-with-youtube-made-exciting",
-      projectSource: "https://github.com/GeekyChakri/Classroom",
-
-      position: "1",
-    },
-    {
-      name: "Shyam Mahanta",
-      twitter: "https://twitter.com/ShyamMahanta2",
-      github: "https://github.com/Angryman18",
-      email: "smahanta118@gmail.com",
-      avatar: "https://avatars.githubusercontent.com/u/63530626?v=4",
-      projectName: "Papercode",
-      projectLink: "https://papercode.netlify.app/",
-      projectSource: "https://github.com/Angryman18/papercode",
-
-      article:
-        "https://blog.reactplay.io/tour-of-a-live-coding-playground-app-built-using-reactjs-nhost",
-      position: "2",
-    },
-    {
-      name: "Aashish Panthi",
-      twitter: "https://twitter.com/aashishpanthi11",
-      github: "https://github.com/aashishpanthi",
-      email: "aashishpanthi11@gmail.com",
-      avatar: "https://avatars.githubusercontent.com/u/60884239?v=4",
-      projectName: "Mailsbe",
-      projectLink: "https://mailsbe.netlify.app/",
-      projectSource: "https://github.com/aashishpanthi/mailsbe",
-      article: "https://blog.reactplay.io/mailsbe-an-email-status-finder",
-      position: "3",
-    },
+    
   ],
   mentions: [
-    {
-      name: "Supriya M",
-      twitter: "https://twitter.com/supminn",
-      github: "https://github.com/supminn",
-      email: "sansup49@gmail.com",
-      avatar: "https://avatars.githubusercontent.com/u/30731236?v=4",
-      projectName: "FinSaver",
-      projectLink: "https://finsaver.vercel.app/",
-      projectSource: "https://github.com/supminn/expense_tracker",
-
-      article: "https://blog.reactplay.io/building-finsaver-for-hack-r-play",
-    },
-    {
-      name: "Armaan Kazi",
-      twitter: "https://twitter.com/Armankazi111",
-      github: "https://github.com/Amyx000/",
-      email: "armankazi111@gmail.com",
-      avatar: "https://avatars.githubusercontent.com/u/104687128?v=4",
-      projectName: "React Resume Builder",
-      projectLink: "https://react--resume--builder.vercel.app/",
-      projectSource: "https://github.com/Amyx000/React-Resume-Builder",
-
-      article:
-        "https://blog.reactplay.io/react-resume-builder-with-react-and-nhost",
-    },
-    {
-      name: "Ammaar Aslam",
-      twitter: "https://twitter.com/itsammaar_7",
-      github: "https://github.com/ammaaraslam",
-      email: "ammaaraslam7@gmail.com",
-      avatar:
-        "https://avatars.githubusercontent.com/u/96367405?s=400&u=cb1c3999b75b33502f3149fd47b251731be286e7&v=4",
-      projectName: "WriteOnce",
-      projectLink: "https://writeonce.vercel.app/",
-      projectSource: "https://github.com/ammaaraslam/write-once",
-
-      article:
-        "https://blog.reactplay.io/introducing-writeonce-a-markdown-editor-for-devs-who-blog",
-    },
+    
   ],
 };
